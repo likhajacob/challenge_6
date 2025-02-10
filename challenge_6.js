44f273ae8b8e7b76d0c5eebc8bd2e3b831fc889b
Likha Jacob <likha.jacob03@gmail.com>
2025-02-09 21:16:43 -0500

    Task 3 - employee bonus calculation

--- challenge_6.js
+++ challenge_6.js
@@ -21,3 +21,19 @@ let calculateSalesTax = function(amount, taxRate) {
 
 console.log("Sales Tax: $", calculateSalesTax(100, 0.07)); // logging of sale for $100 with sales tax of 7% in console
 console.log("Sales Tax: $", calculateSalesTax(500, 0.1)); // logging of sale for $500 with sales tax of 10% in conole
+// Task 3 Arrow Function
+
+let calculateBonus = (salary, performanceRating) => {
+    let bonus;
+    if (performanceRating === "Excellent") {
+        bonus = salary * 0.20; // Bonus is 20% of salary
+    } else if (performanceRating === "Good") {
+        bonus = salary * 0.10; // Bonus is 10% of salary
+    } else if (performanceRating === "Average") {
+        bonus = salary * 0.05; // Bonus is 5% of salary
+    }
+    return bonus;
+};
+
+console.log("Bonus 1: $", calculateBonus (5000, "Excellent")); // logging bonus with 5000 salary and excellent performance rating
+console.log("Bonus 2: $", calculateBonus (7000, "Good")); // logging bonus with 7000 salary and good performance rating
